2dd1e613b9f0f29bce00ce8444123703ab66cfeb
MauroRguez <mrodriguez1214@hotmail.com>
2025-04-23 14:57:14 -0500

    feat post_facebook_react

---
+++ src/components/commentform.jsx
@@ -0,0 +1,8 @@
+
+let CommentForm=() =>   {
+    return (
+      <h1>Componente formulario</h1>
+    );
+  };
+  
+  export default CommentForm;
---
+++ src/components/listcomments.jsx
@@ -0,0 +1,8 @@
+
+let ListComment=() =>   {
+    return (
+      <h1>Listado de comentario</h1>
+    );
+  };
+  
+  export default ListComment;
